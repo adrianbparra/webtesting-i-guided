3a6dfea1e055cae5a94f8518303586371517a9be
adrianbparra <adrianplane12@gmail.com>
2020-04-27 21:15:06 -0700

    added test for add on calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,6 +2,17 @@ module.exports = {
   add,
 };
 
-function add() {
-  return null;
+
+function add(args) {
+
+  const values = Array.isArray(args) ? args : Array.from(arguments);
+  
+    if( !values.every(num => !isNaN(num)) ){
+      throw new Error("not a number")
+    }
+  
+    return values.reduce((sum,num)=> {
+      return sum+num
+    },0);
+    
 }
--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -1,3 +1,39 @@
 const { add } = require('./calculator.js');
 
 // test away!
+
+describe("arithmatic functions", () =>{
+    describe("add", () =>{
+        it("should return the sum of 2 numbers", ()=>{
+            expect(add(2,2)).toBe(4);
+        })
+
+        it("should throw an exception if the params are not numbers", () => {
+            expect(()=>{add("blue", 3)}).toThrow()
+            expect(()=>{add(3, "green")}).toThrow()
+        })
+
+        it("should be a 0 if no prams are included", () =>{
+            const expected = 0;
+
+            const actualresult = add()
+            expect(actualresult).toBe(expected)
+        })
+
+
+        it("should return param if only one is provided",  () => {
+            const expected = 4;
+
+            const actualresult = add(4);
+
+            expect(actualresult).toBe(expected)
+        })
+
+        it("should return addition when arg is an array", () =>{
+            const expected = 12
+            const actualresult=  add([3,5,4])
+
+            expect(actualresult).toBe(expected)
+        })
+    })
+})
